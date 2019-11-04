577e087cdf0b5e091db8fb7131276c2365211541
Cilvia333 <cilvia333x@gmail.com>
2019-11-04 15:19:24 +0900

    [add] top page追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,5 @@
 <template lang="pug">
 div.default
-	Header
 	transition
 		nuxt
 </template>
@@ -16,3 +15,59 @@ export default createComponent({
 	setup(_, _ctx) {}
 })
 </script>
+
+<style lang="scss">
+html {
+	font-family: 'Futura', Arial, Verdana, 'Noto Sans JP', '游ゴシック',
+		'Yu Gothic', '游ゴシック体', 'YuGothic', 'ヒラギノ角ゴ W3',
+		'Hiragino Kaku Gothic', 'Meiryo UI', 'メイリオ', Meiryo, 'ＭＳ Ｐゴシック',
+		'MS PGothic', sans-serif;
+
+	font-size: 16px;
+	word-spacing: 1px;
+	box-sizing: border-box;
+}
+
+main {
+	height: calc(100vh - 160px);
+	margin: 80px 55px;
+}
+
+.container {
+	height: 100%;
+	max-width: 1024px;
+	margin: 0 auto;
+}
+
+li {
+	list-style-type: none;
+}
+
+a {
+	text-decoration: none;
+}
+
+.title {
+	font-size: 20px;
+	font-weight: $bold;
+}
+
+h1 {
+	margin: 0;
+	font-size: 6rem;
+	font-weight: 800;
+}
+
+h2 {
+	margin: 0;
+	font-size: 2.5rem;
+	font-weight: 400;
+	line-height: 1;
+}
+
+h3 {
+	margin: 0;
+	font-size: 1.5rem;
+	font-weight: 400;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,25 @@
 <template lang="pug">
 main.index
-  .index-discription
-    h1.title 2-C-10 Kairi Shiomi
-    p.text とりあえず適当に作っています。許してくれ。
-  .index-items
-    h1.title 
-    ul
-      li.index-item: nuxt-link.index-link.link(to="/task1") 対になる絵
-      li.index-item: nuxt-link.index-link.link(to="/task3") 道具の調査レポート
+  .container
+    .index-logo
+      h1.index-logo-title GUI表現研究
+      h2.index-logo-subtitle GUI Expression <br>
+        | Research
+    .index-name
+      h3.index-name-text DI-2-C-21 塩見海怜
+    ul.index-links 
+      li.index-links-item
+          .index-link-index task1
+          nuxt-link.index-link.link(to="/task1") 対になる絵
+      li.index-links-item
+          .index-link-index task2
+          nuxt-link.index-link.link(to="/task3") none
+      li.index-links-item
+          .index-link-index task3
+          nuxt-link.index-link.link(to="/task3") 道具の調査レポート
+      li.index-links-item
+          .index-link-index task4
+          nuxt-link.index-link.link(to="/task3") none
 </template>
 
 <script lang="ts">
@@ -25,7 +37,59 @@ export default createComponent({
 </script>
 
 <style lang="scss" scoped>
-.index-items {
-	margin-top: 30px;
+.container {
+	position: relative;
+}
+
+.index-logo {
+	margin-top: 0px;
+	position: relative;
+
+	&::after {
+		content: '';
+		position: absolute;
+		bottom: -20px;
+		left: 3px;
+		height: 6px;
+		width: 1.8rem;
+		background-color: $black;
+	}
+}
+
+.index-name {
+	position: absolute;
+	bottom: 0;
+	left: 0;
+}
+
+.index-links {
+	position: absolute;
+	bottom: 0;
+	right: 0;
+	margin: 0;
+
+	display: flex;
+	flex-direction: column;
+	align-items: flex-end;
+
+	.index-links-item {
+		display: inline-block;
+		text-align: right;
+		margin-top: 80px;
+		position: relative;
+
+		.index-link-index {
+			position: absolute;
+			top: -1.8rem;
+			left: 0;
+			font-size: 2rem;
+		}
+		.index-link {
+			display: inline-block;
+			color: $black;
+			background: $gray;
+			font-size: 3rem;
+		}
+	}
 }
 </style>
